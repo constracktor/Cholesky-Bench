6bc4d203bd0280b9658f7a2793a62f76601665ce
constracktor <74077030+constracktor@users.noreply.github.com>
2026-04-16 18:03:26 +0200

    Increase loop count

--- openmp/run.sh
+++ openmp/run.sh
@@ -20,7 +20,7 @@ spack load llvm@22.1.2
 export LD_LIBRARY_PATH=$(spack location -i llvm@22.1.2)/lib/x86_64-unknown-linux-gnu:$LD_LIBRARY_PATH
 # Run executable
 srun --cpu-bind=cores "$SCRIPT_DIR/build/cholesky_openmp" \
-    --loop 1 \
+    --loop 20 \
     --size_start 65536 \
     --size_stop 65536 \
     --tiles_start 4 \
